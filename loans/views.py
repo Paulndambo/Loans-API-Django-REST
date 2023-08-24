84e43a51bbe174fe27841d0f7a707ab5399cdd60
Paul <paulkadabo@gmail.com>
2023-08-24 08:00:41 +0300

    all local changes

--- loans/views.py
+++ loans/views.py
@@ -12,21 +12,21 @@ from django.db import transaction
 from decimal import Decimal
 # Create your views here.
 class LoanModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'put', 'patch', 'delete']
+    #permission_classes = [IsAuthenticated]
+    #http_method_names = ['get', 'put', 'patch', 'delete']
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
 
 class LoanPaymentModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'post']
     queryset = LoanPayment.objects.all()
     serializer_class = LoanPaymentSerializer
 
 
 class LoanApplicationModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
     queryset = LoanApplication.objects.all()
     serializer_class = LoanApplicationSerializer
 
--- people/urls.py
+++ people/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .import views
 from rest_framework.routers import DefaultRouter
+
 router = DefaultRouter()
 router.register("borrowers", views.BorrowerModelViewSet, basename="borrowers")
 router.register("guarantors", views.GuarantorModelViewSet, basename="gurantors")
